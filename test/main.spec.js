7a6edb78a7333116bab4f567126b06c88abb7c54
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-08-13 11:54:24 -0400

    Add tests, decorateError callback.

--- test/main.spec.js
+++ test/main.spec.js
@@ -6,16 +6,17 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import crypto from 'crypto';
 import express from 'express';
+import * as JWT from '@digitalbazaar/minimal-jwt';
 
 import {authorizeAccessToken} from '../lib';
 import {_assertScope} from '../lib/authorizeAccessToken.js';
-import {InvalidRequest} from '@interop/oauth2-errors';
-import base64url from 'base64url-universal';
-const DEFAULT_ALLOWED_JWT_ALGS = new Set(['HS256', 'ES256']);
 
 chai.use(chaiHttp);
 chai.should();
 const {expect} = chai;
+
+const SECRET = '<the-best-kept-secret>';
+const MOCK_CLIENT_ID = 's6BhdRkqt3';
 
 const INVALID_MOCK_ACCESS_TOKEN = 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijc3In0' +
   '.eyJpc3MiOiJodHRwOi8vYXV0aG9yaXphdGlvbi1zZXJ2ZXIuZXhhbXBsZS5jb20iLCJzdW' +
@@ -23,84 +24,29 @@ const INVALID_MOCK_ACCESS_TOKEN = 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ijc3In0' +
   'mh0dHBzOi8vY2FsLmV4YW1wbGUuY29tLyJ9.nNWJ2dXSxaDRdMUKlzs-cYI' +
   'j8MDoM6Gy7pf_sKrLGsAFf1C2bDhB60DQfW1DZL5npdko1_Mmk5sUfzkiQNVpYw';
 
-const VALID_MOCK_ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9' +
-  '.eyJpc3MiOiJodHRwczovL2lzc3Vlci5leGFtcGxlLmNvbSIsImlhdCI6MTYxMzY1NTAyM' +
-  'CwiZXhwIjoxNjQ1MTkxMDIwLCJhdWQiOiIiLCJzdWIiOiIiLCJjbGllbnRfaWQiOiJzNkJoZFJ' +
-  'rcXQzIiwic2NvcGUiOiJyZWFkIHdyaXRlIn0.Hv3I-bgcsxn4IaCmU0dcywaP8Sw' +
-  '_LBLrEgA_pSVc5zM';
-
-function _createVerifyFn({tokenizer}) {
-  return async function verifySignature({
-    alg, kid, data, signature, ALLOWED_JWT_ALGS = DEFAULT_ALLOWED_JWT_ALGS
-  }) {
-    if(!ALLOWED_JWT_ALGS.has(alg)) {
-      throw new InvalidRequest({
-        description: `"${alg}" is invalid.`
-      });
-    }
-    let hmac;
-    try {
-      ({hmac} = await tokenizer.get({id: kid}));
-    } catch(e) {
-      throw new InvalidRequest({
-        description: `Unable to verify token with kid: "${kid}"`
-      });
-    }
-
-    const encodedSignature = base64url.encode(signature);
-    return hmac.verify({data, signature: encodedSignature});
-  };
+async function signFn({data}) {
+  return crypto.createHmac('sha256', Buffer.from(SECRET)).update(data).digest();
 }
 
-async function hashClientSecret({clientSecret}) {
-  assert.string(clientSecret, 'clientSecret');
-  const digest = crypto.createHash('sha256').update(clientSecret).digest();
-  // format as multihash digest
-  // sha2-256: 0x12, length: 32 (0x20), digest value
-  const mh = Buffer.alloc(34);
-  mh[0] = 0x12;
-  mh[1] = 0x20;
-  mh.set(digest, 2);
-  return mh;
-}
+async function verifyFn({alg, kid, data, signature}) {
+  if(alg === 'HS256') {
+    const expectedSignature = await signFn({data});
 
-const mockTokenizer = {
-  async get({id, alg}) {
-    return {
-      id,
-      hmac: {
-        // eslint-disable-next-line max-len
-        id: 'https://localhost:18443/kms/keystores/z19wuB5GSxny6xVDsLgaAibd6/keys/z19rQ9Hjr4tCUSSQTmDiTE771',
-        type: 'Sha256HmacKey2019',
-        algorithm: alg,
-        invocationSigner: {
-          // eslint-disable-next-line max-len
-          id: 'did:key:z6Mkj582WrF84bRJKSFjn9QkEAbUY346Qh7EhHoMoqE7175P#z6Mkj582WrF84bRJKSFjn9QkEAbUY346Qh7EhHoMoqE7175P',
-          type: 'Ed25519VerificationKey2018'
-        },
-        kmsClient: {
-          // eslint-disable-next-line max-len
-          keystore: 'https://localhost:18443/kms/keystores/z19wuB5GSxny6xVDsLgaAibd6',
-          httpsAgent: {}
-        },
-        cache: {},
-        _pruneCacheTimer: null,
-        // eslint-disable-next-line no-unused-vars
-        verify: ({data, signature}) => {
-          return true;
-        }
-      }
-    };
+    return crypto.timingSafeEqual(
+      Buffer.from(signature),
+      Buffer.from(expectedSignature)
+    );
+  } else {
+    throw new Error(`Key "${kid}" is not supported.`);
   }
-};
+}
 
-// eslint-disable-next-line no-unused-vars
-const mockLoadClientRegistration = async ({clientId = 's6BhdRkqt3'}) => {
+const mockLoadClientRegistration = async ({client_id}) => {
+  if(client_id !== MOCK_CLIENT_ID) {
+    throw new Error('Not found.');
+  }
   return {
-    client_id: clientId,
-    client_secret_hash: await hashClientSecret({
-      clientSecret: 'testClientSecret'
-    }),
+    client_id,
     scope: 'read write'
   };
 };
@@ -117,18 +63,46 @@ describe('authorizeAccessToken()', () => {
       validateClaims: async ({claims}) => {
         // perform custom validation on access token claims here
       },
-      verifySignature: _createVerifyFn({tokenizer: mockTokenizer}),
+      verifySignature: verifyFn,
       // logger: console
     }),
     // eslint-disable-next-line no-unused-vars
     (req, res, next) => {
-      res.send('OK');
+      res.json(
+        req.clientMetadata
+      );
     }
   );
 
   let requester;
+  let validAccessToken;
+  let unregisteredClientToken;
 
   before(async () => {
+    validAccessToken = await JWT.sign({
+      header: {typ: 'JWT', alg: 'HS256', kid: '194B72684'},
+      payload: {
+        iss: 'https://issuer.example.com',
+        iat: 1613655020,
+        exp: 1645191020,
+        client_id: MOCK_CLIENT_ID,
+        scope: 'read write'
+      },
+      signFn
+    });
+
+    unregisteredClientToken = await JWT.sign({
+      header: {typ: 'JWT', alg: 'HS256', kid: '194B72684'},
+      payload: {
+        iss: 'https://issuer.example.com',
+        iat: 1613655020,
+        exp: 1645191020,
+        client_id: '<unregistered client id>',
+        scope: 'read write'
+      },
+      signFn
+    });
+
     requester = chai.request(app).keepOpen();
   });
 
@@ -148,7 +122,7 @@ describe('authorizeAccessToken()', () => {
 
   it('should error if authorization header is invalid', async () => {
     const res = await requester.post('/api/example')
-      .set('authorization', `notBearer ${VALID_MOCK_ACCESS_TOKEN}`)
+      .set('authorization', `notBearer ${validAccessToken}`)
       .send({});
     expect(res).to.have.status(400);
     expect(res).to.be.json;
@@ -168,12 +142,30 @@ describe('authorizeAccessToken()', () => {
       'Invalid access token. The access token could not be verified.');
   });
 
+  it('should error if client id belongs to unregistered client', async () => {
+    const res = await requester.post('/api/example')
+      .set('authorization', `Bearer ${unregisteredClientToken}`)
+      .send({});
+    expect(res).to.have.status(400);
+    expect(res).to.be.json;
+    expect(res.body.error).to.equal('invalid_request');
+    expect(res.body.error_description).to.equal(
+      'Could not load a registered agent for the provided client ID.');
+  });
+
+  it('should error if issuer is not authorized', async () => {
+  });
+
+  it('should error if token expired', async () => {
+  });
+
   it('should successfully authorize access token', async () => {
     const res = await requester.post('/api/example')
       .set('content-type', 'application/json')
-      .set('authorization', `Bearer ${VALID_MOCK_ACCESS_TOKEN}`)
+      .set('authorization', `Bearer ${validAccessToken}`)
       .send({});
     expect(res).to.have.status(200);
+    expect(res.body.iss).to.equal('https://issuer.example.com');
   });
 });
 
